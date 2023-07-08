9d45be1281cee33208fb3844defc792c3ae388b6
Tyom5555 <artyom.shahbazyan05gmail.com>
2023-07-08 03:07:06 -0700

    Create component folder

---
+++ src/Components/CardItem/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function cardItem() {
+  return (
+    <div>CardItem</div>
+  )
+}
---
+++ src/DummyPorducts.js
@@ -0,0 +1,19 @@
+export const DUMMYPRODUCTS = [
+    {id:1, name: "peperoni", category: "Pizza", price:3000, photo:"images/pizza.png"},
+    {id:1, name: "mrgain", category: "Pizza", price:1000, photo:"images/pizza.png"},
+    {id:1, name: "sulugumi", category: "Pizza", price:4500, photo:"images/pizza.png"},
+    {id:1, name: "zetunov", category: "Pizza", price:9000, photo:"images/pizza.png"},
+    {id:2, name: "Yoopi", category: "Drin", price:300, photo:"images/drink.webp"},
+    {id:2, name: "Mirinda", category: "Drin", price:300, photo:"images/drink.webp"},
+    {id:2, name: "CocaColla", category: "Drin", price:300, photo:"images/drink.webp"},
+    {id:2, name: "Mirinda", category: "Drin", price:250, photo:"images/drink.webp"},
+    {id:2, name: "Mazz", category: "Drin", price:9000, photo:"images/drink.webp"},
+    {id:3, name: "Arqayaqndzor", category: "", price:1500, photo:"images/fruit.webp"},
+    {id:3, name: "Narinj", category: "Fruits", price:700, photo:"images/fruit.webp"},
+    {id:3, name: "Dzmeruk", category: "Fruits", price:250, photo:"images/fruit.webp"},
+    {id:3, name: "Xaxox", category: "Fruits", price:550, photo:"images/fruit.webp"},
+    {id:3, name: "kartol", category: "Fruits", price:250, photo:"images/fruit.webp"},
+    {id:4, name: "Duet", category: "Coffi", price:100, photo:"images/duet.png"},
+    {id:4, name: "Caputino", category: "Coffi", price:1500, photo:"images/duet.png"},
+    {id:4, name: "Expreso", category: "Coffi", price:700, photo:"images/duet.png"}
+]
